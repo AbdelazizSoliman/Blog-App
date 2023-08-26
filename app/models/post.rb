b0e20339ca3eabecccda32f066cc3ad682779cf2
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-08-26 14:02:41 +0300

    Fix linter errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,10 +14,10 @@ class Post < ApplicationRecord
   # Callbacks
   after_save :update_user_posts_counter
 
-    # Validations
-    validates :title, presence: true, length: { maximum: 250 }
-    validates :comments_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
-    validates :likes_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  # Validations
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 
   # Methods
   def update_user_posts_counter
