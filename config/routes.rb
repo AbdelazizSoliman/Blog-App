97480ed97608ef184fcefda4e8bb7d67db312aa0
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-08-31 22:34:27 +0300

    Update routers

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :comments
-  resources :likes
-  resources :posts
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'users#index'
+  get 'users', to: 'users#index'
+  get 'users/:id', to: 'users#show'
+  get 'users/:id/posts', to: 'posts#index'
+  get 'users/:id/posts/:id', to: 'posts#show'
 end
