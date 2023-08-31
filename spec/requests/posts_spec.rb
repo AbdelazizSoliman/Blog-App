7e81dbb2d06982ce02dca5d7014490ace123982f
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-09-01 02:46:40 +0300

    Update post_spec.rb

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,10 +1,42 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
-  describe "GET /posts" do
-    it "works! (now write some real specs)" do
-      get posts_index_path
-      expect(response).to have_http_status(200)
+RSpec.describe 'PostsController', type: :request do
+  let(:user) { User.create(name: 'Test User') }
+  let(:post) { Post.create(author: user, title: 'Test Post') }
+
+  describe 'GET /index' do
+    before do
+      get user_posts_path(user)
+    end
+
+    context 'renders the index template' do
+      it 'response status is correct' do
+        expect(response).to have_http_status(200)
+      end
+      it 'correct template is rendered' do
+        expect(response).to render_template(:index)
+      end
+      it 'the response body includes correct placeholder text' do
+        expect(response.body).to include('Posts')
+      end
+    end
+  end
+
+  describe 'GET #show' do
+    before do
+      get user_post_path(user, post)
+    end
+
+    context 'renders the show template' do
+      it 'response status is correct' do
+        expect(response).to have_http_status(200)
+      end
+      it 'correct template is rendered' do
+        expect(response).to render_template(:show)
+      end
+      it 'the response body includes correct placeholder text' do
+        expect(response.body).to include('Post number')
+      end
     end
   end
 end
