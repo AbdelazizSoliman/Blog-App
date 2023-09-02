0fbd10ae935b855b2453efe2a481c13912550ae0
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-09-02 12:52:49 +0300

    Creates a Post on behalf of the current_user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,20 @@ class PostsController < ApplicationController
 
   def show; end
 
-  def new; end
+  def new
+    @user = current_user
+    @post = @user.posts.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      flash[:notice] = 'Post created successfully.'
+      redirect_to user_path(current_user)
+    else
+      render 'new'
+    end
+  end
 
   def find_user
     @user = User.find(params[:user_id])
