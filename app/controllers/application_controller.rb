2b2218c0c8e5e8082af4cba1de099ff26db48701
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-09-02 18:15:45 +0300

    Fix rubocop errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,8 @@
 # This is a top-level documentation comment for the class.
 class ApplicationController < ActionController::Base
+  def current_user
+    @current_user ||= User.first
+  end
 
-    def current_user
-        @current_user ||= User.first
-      end
-    
-      helper_method :current_user
+  helper_method :current_user
 end
