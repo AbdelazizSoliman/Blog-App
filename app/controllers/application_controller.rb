14e9639887d32ba79a09bf83407280a394a14043
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-09-02 12:33:52 +0300

    AAdd current user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 # This is a top-level documentation comment for the class.
 class ApplicationController < ActionController::Base
+
+    def current_user
+        @current_user ||= User.first
+      end
+    
+      helper_method :current_user
 end
