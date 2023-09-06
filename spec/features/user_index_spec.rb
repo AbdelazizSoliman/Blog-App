5af27514e896225299ca631396946ef2f2a0d93d
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-09-06 18:48:42 +0300

    Update features tests

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -1,34 +1,28 @@
 require 'rails_helper'
 
-RSpec.feature "User Index", type: :feature do
-  let!(:user1) { create(:user, name: 'Alex', photo: 'https://www.goodfreephotos.com/albums/bolivia/other-bolivia/mountains-and-lake-landscape-scenic.jpg') }
-  let!(:user2) { create(:user, name: 'Abdelaziz', photo: 'https://www.goodfreephotos.com/albums/bolivia/other-bolivia/mountains-and-lake-landscape-scenic.jpg') }
-  let!(:user3) { create(:user, name: 'Ronald') }
-
+RSpec.feature 'User Index', type: :feature do
   scenario 'visiting the user index page' do
+    User.create(name: 'Alex', photo: 'https://www.goodfreephotos.com/albums/bolivia/other-bolivia/mountains-and-lake-landscape-scenic.jpg')
+    User.create(name: 'Abdelaziz', photo: 'https://www.goodfreephotos.com/albums/bolivia/other-bolivia/mountains-and-lake-landscape-scenic.jpg')
+
     visit users_path
 
-    expect(page).to have_content("Alex")
-    expect(page).to have_content("Abdelaziz")
+    expect(page).to have_content('Alex')
+    expect(page).to have_content('Abdelaziz')
     expect(page).to have_css("img[alt='Alex']", count: 1)
     expect(page).to have_css("img[alt='Abdelaziz']", count: 1)
   end
 
-  scenario 'visiting the user index page, you see the number of posts each user has written' do
-    create(:post, author: user1, title: 'first post')
-    create(:post, author: user1, title: 'second post')
-    create(:post, author: user1, title: 'third post')
+  scenario 'visiting the user index page, you see the number of posts each user has written..' do
+    user1 = User.create(name: 'Alex')
+    User.create(name: 'Abdelaziz')
+    Post.create(author: user1, title: 'first post')
+    Post.create(author: user1, title: 'second post')
+    Post.create(author: user1, title: 'third post')
 
     visit users_path
 
-    expect(page).to have_content("3")
-    expect(page).to have_content("0")
-  end
-
-  scenario 'clicking on a user redirects to their show page' do 
-    visit users_path
-    click_link 'Ronald'
-
-    expect(page).to have_current_path(user_posts_path(user3))
+    expect(page).to have_content('3')
+    expect(page).to have_content('0')
   end
 end
--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -1,41 +1,42 @@
 require 'rails_helper'
 
-RSpec.feature "User Show", type: :feature do
+RSpec.feature 'User Show', type: :feature do
+  let(:user) { User.create(name: 'Alex', photo: 'https://www.goodfreephotos.com/albums/bolivia/other-bolivia/mountains-and-lake-landscape-scenic.jpg', bio: 'He is a good programmar') }
+  let!(:post1) { Post.create(author: user, title: 'first post', text: 'first text') }
+  let!(:post2) { Post.create(author: user, title: 'second post', text: 'second text') }
+  let!(:post3) { Post.create(author: user, title: 'third post', text: '3 text') }
+  let!(:post4) { Post.create(author: user, title: '4 post', text: '4 text') }
+
   scenario 'visiting the user Show page' do
-    user1 = User.create(name: 'Alex', photo: 'https://www.goodfreephotos.com/albums/bolivia/other-bolivia/mountains-and-lake-landscape-scenic.jpg')
+    visit user_path(user)
 
-    visit user_path(user1)
-
-    expect(page).to have_content("Alex")
-    expect(page).to have_css("img[alt='Alex']", count: 1)    
+    expect(page).to have_content('Alex')
+    expect(page).to have_css("img[alt='Alex']", count: 1)
   end
 
   scenario 'visiting the user show page, you see the number of posts the user has written..' do
-    user1 = User.create(name: 'Alex')
-    post1 = Post.create(author: user1, title: 'first post')
-    post2 = Post.create(author: user1, title: 'second post')
-    post3 = Post.create(author: user1, title: 'third post')
+    visit user_path(user)
 
-    visit user_path(user1)
-
-    expect(page).to have_content("3 posts")
+    expect(page).to have_content('4 posts')
   end
-  
+
   scenario 'visiting the user show page, you see the 3 most recent post and bio of the user has written..' do
-    user1 = User.create(name: 'Alex', bio: "He is a good programmar")
-    post1 = Post.create(author: user1, title: 'first post', text: 'first text')
-    post2 = Post.create(author: user1, title: 'second post', text: 'second text')
-    post3 = Post.create(author: user1, title: 'third post',text: '3 text' )
-    post4 = Post.create(author: user1, title: '4 post',text: '4 text')
+    visit user_path(user)
+    expect(page).to have_content('He is a good programmar')
+    expect(page).not_to have_content('fist text')
+  end
 
+  scenario 'has a link to the user index page' do
+    visit user_path(user)
 
-    visit user_path(user1)
+    expect(page).to have_button('See all posts')
+    click_link 'See all posts'
+    expect(current_path).to eq(user_posts_path(user))
+  end
 
-    expect(page).to have_content("He is a good programmar")
-    expect(page).to have_content('second text')
-    expect(page).not_to have_content("fist text")
-    expect(page).to have_content("3 text")
-    expect(page).to have_content("4 text")
-
+  scenario 'clicking a user post redirects to post show page' do
+    visit user_path(user)
+    click_link 'second text' # Adjust this link text to match your actual post's content
+    expect(current_path).to eq(user_post_path(user, post2))
   end
 end
