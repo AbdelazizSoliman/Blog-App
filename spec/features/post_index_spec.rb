599c16c4e6b6c241be34d41093f5b0ffa63beb59
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-09-06 23:27:59 +0300

    Fix post_index_spec.rb linter errors

--- spec/features/post_index_spec.rb
+++ spec/features/post_index_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature 'Post Index', type: :feature do
-  let(:user) { User.create(name: 'Abdelaziz', photo: 'https://www.goodfreephotos.com/albums/bolivia/other-bolivia/mountains-and-lake-landscape-scenic.jpg', bio: 'He is a good programmar') }
+  let(:user) { User.create(name: 'Abdelaziz', photo: 'https://www.goodfreephotos.com/albums/bolivia/other-bolivia/mountains-and-lake-landscape-scenic.jpg', bio: 'He is a good programmer') }
   let!(:post) { Post.create(author: user, title: "first post's title", text: 'first text') }
   let!(:comment1) { Comment.create(author: user, post:, text: 'first comment') }
   let!(:comment2) { Comment.create(author: user, post:, text: 'second comment') }
@@ -17,7 +17,7 @@ RSpec.feature 'Post Index', type: :feature do
     expect(page).to have_content('Likes: 1')
   end
 
-  scenario "see some of the post's title, body and first comments" do
+  scenario "see some of the post's title, body, and first comments" do
     visit user_posts_path(user)
     expect(page).to have_content('first text')
     expect(page).to have_content('first comment')
