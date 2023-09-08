bde3b74b10a2d19fa26267d24190f7d2e27512d9
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-09-08 21:39:51 +0300

    Add name as required in registeration, Add logout button

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
-  devise :database_authenticatable, :registerable, :confirmable, :recoverable, :rememberable, stretches: 13
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, stretches: 13
 
 
   # Associations
