76f62d987124b15a3bb955eaa362e48428bf328b
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-09-08 17:23:17 +0300

    Permit additional parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # This is a top-level documentation comment for the class.
 class ApplicationController < ActionController::Base
- 
+
+  before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
