8489c1f2cef903d8ee1e9f3e2da2e407bd8a830c
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-09-08 04:42:53 +0300

    Combine two test cases in user_show to be one to avoid rubocop errors

--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -1,29 +1,29 @@
 require 'rails_helper'
 
 RSpec.feature 'User Show', type: :feature do
-  let(:user) { User.create(name: 'Alex', photo: 'https://www.goodfreephotos.com/albums/bolivia/other-bolivia/mountains-and-lake-landscape-scenic.jpg', bio: 'He is a good programmer') }
+  let(:user) { User.create(name: 'Tom', photo: 'https://www.kasandbox.org/programming-images/avatars/leaf-blue.png', bio: 'He is a good programmar') }
   let!(:post1) { Post.create(author: user, title: 'first post', text: 'first text') }
   let!(:post2) { Post.create(author: user, title: 'second post', text: 'second text') }
+  let!(:post3) { Post.create(author: user, title: 'third post', text: '3 text') }
+  let!(:post4) { Post.create(author: user, title: '4 post', text: '4 text') }
+
   scenario 'visiting the user Show page' do
     visit user_path(user)
 
-    expect(page).to have_content('Alex')
-    expect(page).to have_css("img[alt='Alex']", count: 1)
+    expect(page).to have_content('Tom')
+    expect(page).to have_css("img[alt='Tom']", count: 1)
   end
+
   scenario 'visiting the user show page, you see the number of posts the user has written..' do
     visit user_path(user)
 
-    expect(page).to have_content('2 post')
+    expect(page).to have_content('4 posts')
   end
 
-  scenario 'visiting the user show page, you see the 3 most recent posts' do
+  scenario 'visiting the user show page, you see the 3 most recent post and bio of the user has written..' do
     visit user_path(user)
-    expect(page).not_to have_content('tenth post')
-  end
-
-  scenario 'visiting the user show page, you see the bio of the user..' do
-    visit user_path(user)
-    expect(page).to have_content('He is a good programmer')
+    expect(page).to have_content('He is a good programmar')
+    expect(page).not_to have_content('fist text')
   end
 
   scenario 'has a link to the user index page' do
