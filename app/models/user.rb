5ed2cf430878ca4009a5c5185e8ac3075c0c1e2f
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-09-08 12:21:20 +0300

    Set up a controller with user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 # This is a top-level documentation comment for the class.
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   # Associations
   has_many :posts, foreign_key: :author_id
   has_many :comments
