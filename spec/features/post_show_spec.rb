8ee8d46f2c43c91ba91fbdef163116043222c068
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-09-08 03:51:50 +0300

    Combine two test cases in post_show to be one to avoid rubocop errors

--- spec/features/post_show_spec.rb
+++ spec/features/post_show_spec.rb
@@ -29,30 +29,14 @@ RSpec.feature 'Post Show', type: :feature do
     expect(page).to have_content('first text')
   end
 
-  scenario 'I can see the username of each commentor' do
-    # Create comments by different users
+  scenario 'I can see the username of each commentor and the comment each commentor left' do
     user2 = User.create(name: 'John')
-    user3 = User.create(name: 'Alice')
     Comment.create(post:, author: user2, text: 'Great post!')
-    Comment.create(post:, author: user3, text: 'Nice content!')
 
     visit user_post_path(user, post)
 
     expect(page).to have_content('Alex')
     expect(page).to have_content('John')
-    expect(page).to have_content('Alice')
-  end
-
-  scenario 'I can see the comment each commentor left' do
-    # Create comments by different users
-    user2 = User.create(name: 'John')
-    user3 = User.create(name: 'Alice')
-    Comment.create(post:, author: user2, text: 'Great post!')
-    Comment.create(post:, author: user3, text: 'Nice content!')
-
-    visit user_post_path(user, post)
-
     expect(page).to have_content('Great post!')
-    expect(page).to have_content('Nice content!')
   end
 end
