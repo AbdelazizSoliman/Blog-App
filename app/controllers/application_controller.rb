eed1ef5a8cefc1e3c24f0a91c6e7ffd1edc37ca2
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-09-09 10:12:01 +0300

    Add confirmable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
-# This is a top-level documentation comment for the class.
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!, :update_allowed_parameters, if: :devise_controller?
 
   protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :email, :password, :password_confirmation)
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, stretches: 13
+  devise :database_authenticatable,:confirmable, :registerable, :recoverable, :rememberable, stretches: 13
 
   # Associations
   has_many :posts, foreign_key: :author_id
@@ -15,6 +15,7 @@ class User < ApplicationRecord
   attribute :bio, :text
   attribute :posts_counter, :integer, default: 0
   attribute :photo, :string
+  
 
   # Validations
   validates :name, presence: true
